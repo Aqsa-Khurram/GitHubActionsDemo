4ed19639c267ef84adfee1f22527e8a648c2bcb3
Aqsa-Khurram <aqsakhurram2006@gmail.com>
2025-10-24 17:26:56 +0500

    Update sum.js

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,22 @@
-//function
+// functions
 function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed");
+  }
+  return a / b;
+}
+
+// export all functions
+module.exports = { sum, sub, mul, div };
