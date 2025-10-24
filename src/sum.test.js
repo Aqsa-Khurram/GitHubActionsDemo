1e3f14ad3ef925237c28da667174c8c2e4ada83e
Aqsa-Khurram <aqsakhurram2006@gmail.com>
2025-10-24 19:39:34 +0500

    Update sum.test.js

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,4 +1,5 @@
-const { sum, sub, mul, div } = require('./sum');
+// calculator.test.js
+const { sum, sub, mul, div, modulus } = require('./calculator');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -16,3 +17,14 @@ test('divides 10 / 2 to equal 5', () => {
   expect(div(10, 2)).toBe(5);
 });
 
+test('throws error when dividing by zero', () => {
+  expect(() => div(10, 0)).toThrow('Division by zero is not allowed');
+});
+
+test('modulus of 10 % 3 equals 1', () => {
+  expect(modulus(10, 3)).toBe(1);
+});
+
+test('throws error when modulus by zero', () => {
+  expect(() => modulus(10, 0)).toThrow('Cannot take modulus by zero');
+});
